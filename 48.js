6360d4c7766006aa3b9c26db2329930a79ad9505
Soumyo Chakravorty <92665254+soumyo2002@users.noreply.github.com>
2022-09-01 15:10:03 +0530

    Add files via upload

---
+++ 48.js
@@ -0,0 +1,13 @@
+//To alert the user we use alert function
+let a = alert("This is to show you how to use alter functionality in javascript");
+
+//alert has undefined return function
+console.log(a);
+
+//prompt used to interact with user 
+let b=prompt("Enter name","Guest");
+
+console.log(b);
+
+let deletep=confirm("Do you want to delete this post?");
+console.log(deletep);
